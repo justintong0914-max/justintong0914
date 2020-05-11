32a21939fadb4e2c8f920cbb6365b7712b32666e
justintong0914-max <64099863+justintong0914-max@users.noreply.github.com>
2020-05-12 00:45:52 +0800

    Add files via upload

---
+++ myself.js
@@ -0,0 +1,27 @@
+ function showscr(){
+	        scr.style.display="block"
+	       
+	      
+	        document.getElementById('btn6').disabled = true;
+	        
+	        }
+	        function closescr(){
+	        scr.style.display="none"
+	       
+	        
+	        document.getElementById('btn6').disabled = false;
+	        
+	        }
+	  
+	        function showscr6(){
+	        scr6.style.display="block"
+	        document.getElementById('btn').disabled = true;
+	        
+	        }
+	        function closescr6(){
+	        scr6.style.display="none"
+	        document.getElementById('btn').disabled = false;
+	       
+	        }
+	    
+
